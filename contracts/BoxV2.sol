abee7e0b6902ab8a985cd4927aa0b1ccf285ac91
Md Antonin Islam <antonin.islam.dev@gmail.com>
2023-01-02 17:16:12 +0600

    Adds BoxV2
    - contract
    - deploy script

---
+++ contracts/BoxV2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+import "./Box.sol";
+
+contract BoxV2 is Box {
+    function increment () external {
+        value += 1;
+    }
+
+    function version() pure public virtual override returns (string memory) {
+        return 'v2!';
+    }
+}
---
+++ scripts/deploy_box_v2.ts
@@ -0,0 +1,20 @@
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+  const BoxV2 = await ethers.getContractFactory("BoxV2");
+  const boxV2 = await upgrades.upgradeProxy(`${process.env.BOX_PROXY_ADDRESS}`, BoxV2);
+  
+  console.log("Contract address:", boxV2.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
